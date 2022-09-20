2f7be4dd9b60c8768aae8412f2eed8d934e294e0
zwh <3177946695@qq.com>
2022-09-20 16:10:41 +0800

    Rouge

--- Assets/Script/BaseMove.cs
+++ Assets/Script/BaseMove.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BaseMove : MonoBehaviour
 {
@@ -14,15 +15,38 @@ public class BaseMove : MonoBehaviour
     public float checkRadius;//检测地面偏移
     public LayerMask whatIsGround;//地面图层
     public int jumpcount;
-
+    public Slider shiftline;
     public bool isGround;//是否在地面
-    // Start is called before the first frame update
+    private bool shift_or_not;
+    private float shifttime;//shift计时器
+
+
+    [Header("显示残影的持续时间")]
+    public float durationTime;
+    [Header("生成残影与残影之间的时间间隔")]
+    public float spawnTimeval;
+    private float spawnTimer;//生成残影的时间计时器
+
+    [Header("残影颜色")]
+    public Color ghostColor;
+    [Header("残影层级")]
+    public int ghostSortingOrder;
+
+    private SpriteRenderer sr;//SpriteRenderer
+    private List<GameObject> ghostList = new List<GameObject>();//残影列表
+    private void Awake()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+
     void Start()
     {
+        shifttime = 0;
+        shift_or_not = true;
         jumpcount = 2;
         toward = 0;
         Player = GameObject.FindGameObjectWithTag("Player");
-        speed = 0.015f;
+        speed = 0.012f;
         rigidbody_of_player = GetComponent<Rigidbody2D>();
         animator_of_player = GetComponent<Animator>();
         collder_of_player = GetComponent<BoxCollider2D>();
@@ -35,16 +59,14 @@ public class BaseMove : MonoBehaviour
     {
         Move();
         Isground();
-        
+
     }
     private void Move()
     {
 
-        rigidbody_of_player.velocity = new Vector2(0, rigidbody_of_player.velocity.y);
-        collder_of_player.isTrigger = false;
         if (Input.GetKey(KeyCode.A))
         {
-            transform.transform.Translate(Vector3.left *speed, Space.Self);
+            transform.transform.Translate(Vector3.left * speed, Space.Self);
             Player.transform.localScale = new Vector3(-1, 1, 1);
             toward = -1;
         }
@@ -55,31 +77,43 @@ public class BaseMove : MonoBehaviour
             toward = 1;
         }
         if (Input.GetKeyDown(KeyCode.Space))
-            {
+        {
             if (jumpcount > 1)
             {
-                rigidbody_of_player.velocity=new Vector2(0,Mathf.Lerp(5, 10, 0.6f));
+                rigidbody_of_player.velocity = new Vector2(0, Mathf.Lerp(5, 10, 0.6f));
                 animator_of_player.Play("Jump");
                 jumpcount--;
             }
-            }
-        if (Input.GetKey(KeyCode.LeftShift)&&toward!=0)
+        }
+        if (Input.GetKey(KeyCode.LeftShift) && toward != 0 && shift_or_not == true)
         {
+
             if (toward == 1)
-            { Player.transform.position = new Vector2(Player.transform.position.x + 0.2f, Player.transform.position.y);
+            {
+                Player.transform.position = new Vector2(Player.transform.position.x + 6f, Player.transform.position.y);
                 animator_of_player.Play("shift");
-                collder_of_player.isTrigger = true;
+
 
             }
             if (toward == -1)
-            { Player.transform.position = new Vector2(Player.transform.position.x - 0.2f, Player.transform.position.y);
-                animator_of_player.Play("shift");
-                collder_of_player.isTrigger = true;
-            }
+            {
 
-            
+                Player.transform.position = new Vector2(Player.transform.position.x - 6f, Player.transform.position.y);
+                animator_of_player.Play("shift");
+            }
+            shift_or_not = false;
+
         }
-        
+        if (shift_or_not == false)
+        {
+            shifttime += Time.deltaTime;
+            shiftline.value = shifttime / 5;
+            if (shifttime >= 5)
+            {
+                shift_or_not = true;
+                shifttime = 0;
+            }
+        }
     }
     private void Isground()
     {
@@ -93,5 +127,48 @@ public class BaseMove : MonoBehaviour
             animator_of_player.SetBool("fall", false);
         }
     }
+    private void DrawGhost()
+    {
+        if (spawnTimer >= spawnTimeval)
+        {
+            spawnTimer = 0;
+
+            GameObject _ghost = new GameObject();
+            ghostList.Add(_ghost);
+            _ghost.name = "ghost";
+            _ghost.AddComponent<SpriteRenderer>();
+            _ghost.transform.position = transform.position;
+            _ghost.transform.localScale = transform.localScale;
+            SpriteRenderer _sr = _ghost.GetComponent<SpriteRenderer>();
+            _sr.sprite = sr.sprite;
+            _sr.sortingOrder = ghostSortingOrder;
+            _sr.color = ghostColor;
+
+            
+        }
+        spawnTimer += Time.deltaTime;
+
+    }
+
+
+    private void Fade()
+    {
+        for (int i = 0; i < ghostList.Count; i++)
+        {
+            SpriteRenderer ghostSR = ghostList[i].GetComponent<SpriteRenderer>();
+            if (ghostSR.color.a <= 0)
+            {
+                GameObject tempGhost = ghostList[i];
+                ghostList.Remove(tempGhost);
+                Destroy(tempGhost);
+            }
+            else
+            {
+                float fadePerSecond = (ghostColor.a / durationTime);
+                Color tempColor = ghostSR.color;
+                tempColor.a -= fadePerSecond * Time.deltaTime;
+                ghostSR.color = tempColor;
+            }
+        }
+    }
 }
-    
