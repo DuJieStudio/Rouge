a21aa6610b666d28ee4198d93a6a6005e027e5cd
XHW <571016314@qq.com>
2022-10-11 16:23:55 +0800

    2022.10.11_xhw

---
+++ Assets/Script/Weapon.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+
+    public GameObject BulletPrefab;
+    public float fireRate = 0.5F;//0.5秒实例化一个子弹
+    private float nextFire = 0.0F;
+
+
+    private void Update()
+    {
+        Shoot();
+    }
+    public void Shoot()
+    {
+        if (Time.time > nextFire)//让子弹发射有间隔
+        {
+            nextFire = Time.time + fireRate;//Time.time表示从游戏开发到现在的时间，会随着游戏的暂停而停止计算。
+            Instantiate(BulletPrefab, transform.position, transform.rotation);
+        }
+    }
+}
